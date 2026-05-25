e6837438e59e19dfefaaa0ba5424cc93f3297d9a
Juan Amcis Paiva <juanpaiva887@gmail.com>
2026-05-25 19:39:33 -0300

    add background tasks in image conversion

--- backend/app/routes/image_converter.py
+++ backend/app/routes/image_converter.py
@@ -1,15 +1,19 @@
-from fastapi import APIRouter, UploadFile, File, HTTPException
+from fastapi import APIRouter, UploadFile, File, HTTPException, BackgroundTasks
 from fastapi.responses import FileResponse
 from app.exceptions.custom_exceptions import ConversionError
 from app.services.image_service import ImageService
+from app.core.utils import remove_file
 
 image_router = APIRouter(prefix="/images", tags=["Imagens"])
 service = ImageService()
 
 @image_router.post("/png-to-pdf")
-async def png_to_pdf(file: UploadFile = File(...)):
+async def png_to_pdf(background_tasks: BackgroundTasks, file: UploadFile = File(...)):
     try:
         output = await service.convert_png_to_pdf(file)
+
+        background_tasks.add_task(remove_file, output)
+
         return FileResponse(output)
     
     except ConversionError as e:
@@ -19,9 +23,12 @@ async def png_to_pdf(file: UploadFile = File(...)):
         )
 
 @image_router.post("/pdf-to-png")
-async def pdf_to_png(file: UploadFile = File(...)):
+async def pdf_to_png(background_tasks: BackgroundTasks, file: UploadFile = File(...)):
     try:
         output = await service.convert_pdf_to_png(file)
+
+        background_tasks.add_task(remove_file,output)
+
         return FileResponse(output)
     
     except ConversionError as e:
@@ -31,10 +38,14 @@ async def pdf_to_png(file: UploadFile = File(...)):
         )
 
 @image_router.post("/jpg-to-png")
-async def jpg_to_png(file: UploadFile = File(...)):
+async def jpg_to_png(background_tasks: BackgroundTasks, file: UploadFile = File(...)):
     try:
         output = await service.convert_jpg_to_png(file)
+
+        background_tasks.add_task(remove_file,output)
+
         return FileResponse(output)
+    
     except ConversionError as e:
         raise HTTPException(
             status_code=500,
