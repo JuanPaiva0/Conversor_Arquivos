ed1574c63d3f0f3a1533de0adc4daa5b9b256406
Juan Amcis Paiva <juanpaiva887@gmail.com>
2026-05-25 16:54:39 -0300

    error handling in spreadsheet conversion

--- backend/app/routes/spreadsheet_converter.py
+++ backend/app/routes/spreadsheet_converter.py
@@ -1,18 +1,32 @@
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, UploadFile, File, HTTPException
 from fastapi.responses import FileResponse
 from app.services.spreadsheet_service import SpreadsheetService
-
+from app.exceptions.custom_exceptions import ConversionError
 
 spreadsheet_router = APIRouter(prefix="/spreadsheet", tags=["Planilhas"])
 service = SpreadsheetService()
 
-
 @spreadsheet_router.post("/csv-to-xlsx")
 async def csv_to_xlsx(file: UploadFile = File(...)):
-    output = await service.convert_csv_to_xlsx(file)
-    return FileResponse(output)
+    try:
+        output = await service.convert_csv_to_xlsx(file)
+        return FileResponse(output)
+    
+    except ConversionError as e:
+        raise HTTPException(
+            status_code=500,
+            detail=str(e)
+        )
 
 @spreadsheet_router.post("/xlsx-to-csv")
 async def xlsx_to_csv(file: UploadFile = File(...)):
-    output = await service.convert_xlsx_to_csv(file)
-    return FileResponse(output)
+    try:
+        output = await service.convert_xlsx_to_csv(file)
+        return FileResponse(output)
+    
+    except ConversionError as e:
+        raise HTTPException (
+            status_code=500,
+            detail=str(e)
+        )
+        
--- backend/app/services/spreadsheet_service.py
+++ backend/app/services/spreadsheet_service.py
@@ -1,3 +1,4 @@
+from app.exceptions.custom_exceptions import ConversionError
 import pandas as pd
 import os
 
@@ -10,23 +11,35 @@ class SpreadsheetService:
         return OUTPUT_DIR
 
     async def convert_csv_to_xlsx(self, file):
-        output_dir = self.ensure_output_dir()
+        try:
+            output_dir = self.ensure_output_dir()
 
-        name, _ = os.path.splitext(file.filename)
-        output_path = os.path.join(output_dir, f"{name}.xlsx")
+            name, _ = os.path.splitext(file.filename)
+            output_path = os.path.join(output_dir, f"{name}.xlsx")
 
-        spreadsheet = pd.read_csv(file.file)
-        spreadsheet.to_excel(output_path, index=False)
+            spreadsheet = pd.read_csv(file.file)
+            spreadsheet.to_excel(output_path, index=False)
 
-        return output_path
-
+            return output_path
+        
+        except Exception as e:
+            raise ConversionError(
+                f"Erro ao converter CSV para XLSX: {str(e)}"
+            ) from e
+        
     async def convert_xlsx_to_csv(self, file):
-        output_dir = self.ensure_output_dir()
+        try:
+            output_dir = self.ensure_output_dir()
 
-        name, _ = os.path.splitext(file.filename)
-        output_path = os.path.join(output_dir, f"{name}.csv")
+            name, _ = os.path.splitext(file.filename)
+            output_path = os.path.join(output_dir, f"{name}.csv")
 
-        spreadsheet = pd.read_excel(file.file)
-        spreadsheet.to_csv(output_path, index=False)
+            spreadsheet = pd.read_excel(file.file)
+            spreadsheet.to_csv(output_path, index=False)
 
-        return output_path
+            return output_path
+        
+        except Exception as e:
+            raise ConversionError(
+                f"Erro ao converter XLSX para CSV: {str(e)}"
+            ) from e
